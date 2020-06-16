a7a97245481d8676f9e16b42b744b6ed5eb3313f
rob <robertahuber88@gmail.com>
2020-06-16 20:29:21 +0200

    new route

--- services/movies.js
+++ services/movies.js
@@ -35,81 +35,111 @@ saveMoviesLOR = async (movies) => {
 };
 
 
-  router.get('/harrypotter', async (req, res) => {
-    var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=harry%20potter&type=movie')
-    var hpMovies = await response.json();
-    delete hpMovies.Search.Response
-    delete hpMovies.Search.totalResults
-    hpMovies = hpMovies.Search
-    await saveMoviesHP(hpMovies)
-    res.send(hpMovies)  
-  });
-    
-    router.get('/hobbit', async (req, res) => {
-    var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=hobbit&type=movie')
-    var hobbitMovies = await response.json();
-    delete hobbitMovies.Search.Response
-    delete hobbitMovies.Search.totalResults
-    hobbitMovies = hobbitMovies.Search
-    // res.status(200).send((hobbitMovies[0]).toString());        
-    await saveMoviesHobbit(hobbitMovies)
-    res.send(hobbitMovies) 
-  });
 
-  router.get('/lor', async (req, res) => {
-    var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=lord%20of%20the%20rings&type=movie')
-    var lor = await response.json();
-    delete lor.Search.Response
-    delete lor.Search.totalResults
-    lor = lor.Search
-    // res.status(200).send((hobbitMovies[0]).toString());        
-    await saveMoviesLOR(lor)
-    res.send(lor) 
-  }); 
+router.get('/top-rated-movies', async (req, res) => {
+  const response = await fetch("https://imdb8.p.rapidapi.com/title/get-top-rated-movies", {
+    headers: { 'Access-Control-Allow-Origin': "*", "x-rapidapi-host": "imdb8.p.rapidapi.com", "x-rapidapi-key": "784a08652cmsha32051208a637acp1f594djsna6648f90d98e" }
+  })
+  let topMovieArr = [];
+  let topTenArr = [];
+  let movieId;
+  let movieIdSplit;
+  let topRatedMovieList = await response.json();
+  topRatedMovieList = topRatedMovieList.slice(0, 10)
 
-  //filteredSearch
-  router.get("/:movieName", async (req, res) => {
-    var misc = []
-    var movieName = req.params.movieName
-    var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=' + movieName)
-    var movies = await response.json();
-    delete movies.Search.Response
-    delete movies.Search.totalResults
-    movies = movies.Search
-    movies.forEach(async movies => {
-     misc.push(movies)
-      })
-    await saveMiscMovies(misc);
-    res.send(misc);
-  });
+  topRatedMovieList.map(movie => (
+    movieId = movie.id,
+    movieIdSplit = movieId.split('/'),
+    movieId = movieIdSplit[2],
+    topMovieArr.push(movieId)
+  ))
 
-    //getMovieDetails
-    router.get("/details/:imdbID", async (req, res) => {
-      var imdbID = req.params.imdbID
-      var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&i=' + imdbID)
-      var movieDetails = await response.json();
-      res.send(movieDetails);
-    });
+  for (let i = 0; i < topMovieArr.length; i++) {
+    const resp = await fetch(`https://imdb8.p.rapidapi.com/title/get-details?tconst=${movieId}`, {
+      headers: { 'Access-Control-Allow-Origin': "*", "x-rapidapi-host": "imdb8.p.rapidapi.com", "x-rapidapi-key": "784a08652cmsha32051208a637acp1f594djsna6648f90d98e" }
+    })
+    topTenArr.push(await resp.json())
+}
+  res.send(topTenArr)
+})
+
+
+
+router.get('/harrypotter', async (req, res) => {
+  var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=harry%20potter&type=movie')
+  var hpMovies = await response.json();
+  delete hpMovies.Search.Response
+  delete hpMovies.Search.totalResults
+  hpMovies = hpMovies.Search
+  await saveMoviesHP(hpMovies)
+  res.send(hpMovies)
+});
+
+router.get('/hobbit', async (req, res) => {
+  var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=hobbit&type=movie')
+  var hobbitMovies = await response.json();
+  delete hobbitMovies.Search.Response
+  delete hobbitMovies.Search.totalResults
+  hobbitMovies = hobbitMovies.Search
+  // res.status(200).send((hobbitMovies[0]).toString());        
+  await saveMoviesHobbit(hobbitMovies)
+  res.send(hobbitMovies)
+});
+
+router.get('/lor', async (req, res) => {
+  var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=lord%20of%20the%20rings&type=movie')
+  var lor = await response.json();
+  delete lor.Search.Response
+  delete lor.Search.totalResults
+  lor = lor.Search
+  // res.status(200).send((hobbitMovies[0]).toString());        
+  await saveMoviesLOR(lor)
+  res.send(lor)
+});
+
+//filteredSearch
+router.get("/:movieName", async (req, res) => {
+  var misc = []
+  var movieName = req.params.movieName
+  var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&s=' + movieName)
+  var movies = await response.json();
+  delete movies.Search.Response
+  delete movies.Search.totalResults
+  movies = movies.Search
+  movies.forEach(async movies => {
+    misc.push(movies)
+  })
+  await saveMiscMovies(misc);
+  res.send(misc);
+});
+
+//getMovieDetails
+router.get("/details/:imdbID", async (req, res) => {
+  var imdbID = req.params.imdbID
+  var response = await fetch('http://www.omdbapi.com/?apikey=448f4427&i=' + imdbID)
+  var movieDetails = await response.json();
+  res.send(movieDetails);
+});
 
 //oneJSONFile
-  router.get("/", async (req, res) => {
-    var allMovies = []
-    var hobbitMovies = await getItems("hobbitMovies.json");
-    var hpMovies = await getItems("hpMovies.json");
-    var lorMovies= await getItems("lordOftheRings.json");
-    hobbitMovies.forEach(async movies => {
-      allMovies.push(movies)
-      })
-      hpMovies.forEach(async movies => {
-        allMovies.push(movies)
-        })
-        lorMovies.forEach(async movies => {
-          allMovies.push(movies)
-          })
+router.get("/", async (req, res) => {
+  var allMovies = []
+  var hobbitMovies = await getItems("hobbitMovies.json");
+  var hpMovies = await getItems("hpMovies.json");
+  var lorMovies = await getItems("lordOftheRings.json");
+  hobbitMovies.forEach(async movies => {
+    allMovies.push(movies)
+  })
+  hpMovies.forEach(async movies => {
+    allMovies.push(movies)
+  })
+  lorMovies.forEach(async movies => {
+    allMovies.push(movies)
+  })
 
-    await saveMovies(allMovies);
-    res.send(allMovies)
-  });
+  await saveMovies(allMovies);
+  res.send(allMovies)
+});
 
 //addMovie
 router.post("/", async (req, res) => {
